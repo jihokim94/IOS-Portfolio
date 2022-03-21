5b3b48d4fd172d7484fac1ac418292cd5ba31c3e
jihoKim94 <kj941225@gmail.com>
2022-03-21 10:34:14 +0900

    docs:수준별 프로젝트 폴더 분류 및 신규프로젝트 생성

---
+++ Projects/Basic/MyAlbum/MyAlbum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by 김지호 on 2022/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
