1021260dc8a324177a3b89bb78c1cc4ca23cac3a
jihoKim94 <kj941225@gmail.com>
2022-03-26 21:23:53 +0900

    #APP4 프로젝트 및 기본 UI 생성

---
+++ Projects/Basic/Todo #APP4/Todo #APP4/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Todo #APP4
+//
+//  Created by 김지호 on 2022/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func tapAddButton(_ sender: Any) {
+    }
+    
+}
+
