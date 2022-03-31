b20276ae6582a0afa3729957612a6c9b455ebfda
jihoKim94 <kj941225@gmail.com>
2022-03-31 23:54:47 +0900

    일기앱 프로젝트과 UI 생성
    APP#5

---
+++ Projects/Basic/MyDiary APP#5/MyDiary APP#5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDiary APP#5
+//
+//  Created by 김지호 on 2022/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
