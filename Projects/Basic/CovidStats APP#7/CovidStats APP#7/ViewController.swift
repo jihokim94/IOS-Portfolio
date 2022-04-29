17ab9134efd1d176838d6064f7ba8d39d0081d3f
jihoKim94 <kj941225@gmail.com>
2022-04-29 17:59:19 +0900

    feature : configure PieChartView
    APP#7

--- Projects/Basic/CovidStats APP#7/CovidStats APP#7/ViewController.swift
+++ Projects/Basic/CovidStats APP#7/CovidStats APP#7/ViewController.swift
@@ -24,19 +24,44 @@ class ViewController: UIViewController {
             switch result {
             case let .success(data) :
                 self.configureStackView(koreaCovidOverView: data.korea)
-                self.configurePieChart()
+                let cityList = self.makeCityOverViewList(covidOverView: data)
+                self.configurePieChart(cityOverViewList : cityList)
             case let .failure(error) :
                 print(error.localizedDescription)
             }
         })
     }
-    
+    private func makeCityOverViewList(covidOverView : CovidOverView) -> [CityOverView] {
+        return [covidOverView.busan,covidOverView.daegu,covidOverView.daegu,covidOverView.ulsan,covidOverView.jeju,covidOverView.jeonbuk,covidOverView.jeonnam,covidOverView.chungbuk,covidOverView.chungnam,covidOverView.incheon,covidOverView.gangwon,covidOverView.gwangju,covidOverView.gyeongbuk,covidOverView.gyeongnam,covidOverView.sejong]
+    }
     private func configureStackView(koreaCovidOverView : CityOverView){
         totalCaseLabel.text = "\(koreaCovidOverView.totalCase)명"
         newCaseLabel.text = "\(koreaCovidOverView.newCase)명"
     }
-    private func configurePieChart() {
-        
+    private func configurePieChart(cityOverViewList : [CityOverView]) {
+        let entries = cityOverViewList.compactMap { [weak self] overview -> PieChartDataEntry? in
+            guard let self = self else {return nil}
+            return PieChartDataEntry(value: self.numberFormmatter(str :overview.newCase), label: overview.countryName)
+        }
+        let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+        dataSet.sliceSpace = 1
+        dataSet.entryLabelColor = .black
+        dataSet.valueTextColor = .black
+        dataSet.xValuePosition = .outsideSlice
+        dataSet.valueLinePart1Length = 0.25
+        dataSet.valueLinePart2Length = 0.35
+        dataSet.colors = ChartColorTemplates.vordiplom()
+            + ChartColorTemplates.joyful()
+            + ChartColorTemplates.colorful()
+            + ChartColorTemplates.liberty()
+            + ChartColorTemplates.pastel()
+            + ChartColorTemplates.material()
+        self.pieChart.data = PieChartData(dataSet: dataSet)
+    }
+    private func numberFormmatter(str : String) -> Double {
+        let f = NumberFormatter()
+        f.numberStyle = .decimal
+        return f.number(from: str)?.doubleValue ?? 0
     }
     
     private func fetchCovidOverView(completionHandler : @escaping (Result<CovidOverView , Error>) -> ()) {
@@ -44,19 +69,19 @@ class ViewController: UIViewController {
         let param = ["serviceKey" : APIKey.key]
         
         AF.request(url, method: .get , parameters: param).responseData { response in
-                    switch response.result {
-                    case let .success(data) :
-                        do {
-                            let decoder = JSONDecoder()
-                            let covidOverView = try decoder.decode(CovidOverView.self, from: data)
-                            completionHandler(.success(covidOverView))
-                        } catch let error {
-                            completionHandler(.failure(error))
-                        }
-                    case let .failure(error) :
-                        completionHandler(.failure(error))
-                    }
-                   }
+            switch response.result {
+            case let .success(data) :
+                do {
+                    let decoder = JSONDecoder()
+                    let covidOverView = try decoder.decode(CovidOverView.self, from: data)
+                    completionHandler(.success(covidOverView))
+                } catch let error {
+                    completionHandler(.failure(error))
+                }
+            case let .failure(error) :
+                completionHandler(.failure(error))
+            }
+        }
     }
     
 }
