2f4ff3f37b09adec3888e93701bd039a0c1259fd
jihoKim94 <kj941225@gmail.com>
2022-05-03 12:37:47 +0900

    feature : firebaseAPP 과 프로젝트 연결

--- Projects/intermediate/LoginAPPWithAuth APP#8/LoginAPPWithAuth APP#8/AppDelegate.swift
+++ Projects/intermediate/LoginAPPWithAuth APP#8/LoginAPPWithAuth APP#8/AppDelegate.swift
@@ -6,31 +6,34 @@
 //
 
 import UIKit
-
+import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // 우리가 만든 프로젝트와 firebase 가 연결 되게 하기
+        FirebaseApp.configure()
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
