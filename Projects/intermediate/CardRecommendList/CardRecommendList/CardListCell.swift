42d271620a8fceba3947da21c9e29dc16eaa34a6
jihoKim94 <kj941225@gmail.com>
2022-05-09 12:12:07 +0900

    feature : google login App finished

---
+++ Projects/intermediate/CardRecommendList/CardRecommendList/CardListCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardListCell.swift
+//  CardRecommendList
+//
+//  Created by 김지호 on 2022/05/09.
+//
+
+import UIKit
+
+class CardListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Projects/intermediate/CardRecommendList/CardRecommendList/CreditCard.swift
@@ -0,0 +1,8 @@
+//
+//  CreditCard.swift
+//  CardRecommendList
+//
+//  Created by 김지호 on 2022/05/09.
+//
+
+import Foundation
